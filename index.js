417dd3e611f849a22e0f8a0e2743bee7ba21b77b
Liza <121138357+lizalelashvili@users.noreply.github.com>
2023-03-04 17:47:24 +0400

    added first assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+// 1
+const currentDay = new Date().getDay();
+// console.log(currentDay)
+
+switch(currentDay){
+    case 0:
+        console.log("Sunday");
+        break;
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+}
+// 2
+
+
